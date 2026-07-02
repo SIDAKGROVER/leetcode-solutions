7add3b60b954b6f9768f073c1e823c8c2b16f07c
SIDAK SINGH <sidaksingh269@gmail.com>
2026-07-02 13:01:02 +0530

    Time: 1 ms (59.82%), Space: 47.9 MB (38.47%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int trap(int[] arr) {
+        int n=arr.length;
+        int l=0,r=n-1;
+        int lmax=0,rmax=0;
+        int total=0;
+
+        while(l<=r){
+            if(arr[l]<=arr[r]){
+                if(lmax>arr[l]){
+                    total+=lmax-arr[l];
+                }
+                else{
+                    lmax=arr[l];
+                }
+                l++;
+            } 
+            else{
+                if(rmax>arr[r]){
+                    total+=rmax-arr[r];
+                }
+                else{
+                    rmax=arr[r];
+                }
+                r--;
+            }
+        }  
+
+        return total;
+
+    }
+}
