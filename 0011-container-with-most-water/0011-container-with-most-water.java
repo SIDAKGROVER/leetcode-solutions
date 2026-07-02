2d115247f99e4315785062524d81eb82fbe4c809
SIDAK SINGH <sidaksingh269@gmail.com>
2026-07-02 20:44:30 +0530

    Time: 5 ms (82.33%), Space: 77.1 MB (92.68%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxArea(int[] arr) {
+        int i=0;
+        int j=arr.length-1;
+        int max=0;
+        while(i<j){
+            int n=(j-i)*Math.min(arr[i],arr[j]);
+            max=Math.max(max,n);
+            if(arr[i]>arr[j])j--;
+            else i++;
+        }
+        return max;
+    }
+}
