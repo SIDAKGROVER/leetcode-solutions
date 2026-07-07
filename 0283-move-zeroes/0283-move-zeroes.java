0644529994f58d58ca2e0127a7c1c7a5e73511bd
SIDAK SINGH <sidaksingh269@gmail.com>
2026-07-07 17:04:31 +0530

    Time: 1 ms (99.84%), Space: 47.7 MB (56.66%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int zero=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[zero]=nums[i];
+                zero++;
+            }
+        }
+        while(zero<n){
+            nums[zero]=0;
+            zero++;
+        }
+    }
+}
