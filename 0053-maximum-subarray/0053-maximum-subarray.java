7452e2c8369b481560b308aa98623b91da793bd3
SIDAK SINGH <sidaksingh269@gmail.com>
2026-07-09 23:35:22 +0530

    Time: 1 ms (99.96%), Space: 77.3 MB (31.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,12 +3,25 @@ class Solution {
         int n=nums.length;
         int sum=0;
         int maxi=Integer.MIN_VALUE;
+
+        //if intervier asked to print also
+        // int start = 0;
+        // int end = 0;
+        // int tempStart = 0;
         for(int i=0;i<n;i++){
+            // if(sum==0)tempStart=i;
             sum+=nums[i];
-            if(sum>maxi)maxi=sum;
+            if(sum>maxi){
+                maxi=sum;
+                // start=tempStart;
+                // end=i;
+            }
             if(sum<0)sum=0;
 
         }
+        // for (int i = start; i <= end; i++) {
+        //     System.out.print(nums[i] + " ");
+        // }
         if(maxi==Integer.MIN_VALUE)return 0;
         return maxi;
     }
