fba43388a4118b9098ed871c2c089d7b1fc66db2
SIDAK SINGH <sidaksingh269@gmail.com>
2026-07-25 17:15:36 +0530

    Time: 4 ms (23.36%), Space: 48.1 MB (64.89%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        for(int i=0;i<n;i++){
+            int s=0;
+            int e=m-1;
+            while(s<=e){
+                int mid=s+(e-s)/2;
+                if(matrix[i][mid]==target)return true;
+                else if(matrix[i][mid]<target){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
