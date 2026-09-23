390e0e302981bff9db091b46bf0a8639e029ec6f
SIDAK SINGH <sidaksingh269@gmail.com>
2026-09-24 03:47:59 +0530

    Time: 1 ms (87.2%), Space: 44.2 MB (38.9%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,17 @@
 class Solution {
-    public void func(int[] nums,int idx,List<Integer>curr,List<List<Integer>>ans){
+    public void func(int idx,int[] nums,List<Integer>curr,List<List<Integer>>ans){
         if(idx==nums.length){
             ans.add(new ArrayList<>(curr));
             return;
         }
         curr.add(nums[idx]);
-        func(nums,idx+1,curr,ans);
+        func(idx+1,nums,curr,ans);
         curr.remove(curr.size()-1);
-        func(nums,idx+1,curr,ans);
+        func(idx+1,nums,curr,ans);
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>ans=new ArrayList<>();
-        func(nums,0,new ArrayList<>(),ans);
+        func(0,nums,new ArrayList<>(),ans);
         return ans;
     }
 }
